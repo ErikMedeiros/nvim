691ca96fa4814d753c20324687a552b8736c8d45
Erik Medeiros <erikmedeiros4@gmail.com>
2025-07-27 17:50:12 -0300

    fix: session loading

--- lua/config/plugins/projections.lua
+++ lua/config/plugins/projections.lua
@@ -1,29 +1,25 @@
 return {
   'gnikdroy/projections.nvim',
   branch = "pre_release",
-  enabled = true,
   config = function()
     require("projections").setup({
       workspaces = {
         { "~/repos",     {} },
-        { "~/repos/gbm", {} }
+        { "~/repos/gbm", {} },
       }
     })
 
-    vim.opt.sessionoptions:append("localoptions")
-
     require('telescope').load_extension('projections')
-    vim.keymap.set("n", "<leader>fp", ":Telescope projections<CR>", { silent = true, desc = "[F]ind [P]roject" })
+    vim.keymap.set("n", "<leader>sp", ":Telescope projections<CR>", { silent = true, desc = "[S]earch [P]rojects" })
 
-    local Session = require("projections.session")
-    vim.api.nvim_create_autocmd({ 'VimLeavePre' }, {
-      callback = function() Session.store(vim.uv.cwd()) end,
+    vim.api.nvim_create_autocmd('VimLeavePre', {
+      callback = function() require("projections.session").store(vim.fn.getcwd()) end,
     })
 
-    local switcher = require("projections.switcher")
-    vim.api.nvim_create_autocmd({ "VimEnter" }, {
+    vim.api.nvim_create_autocmd("VimEnter", {
+      nested = true,
       callback = function()
-        if vim.fn.argc() == 0 then switcher.switch(vim.loop.cwd()) end
+        if vim.fn.argc() == 0 then require("projections.switcher").switch(vim.fn.getcwd()) end
       end,
     })
   end
